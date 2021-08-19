f89f67347ea7b2208768ea88032f82bb01371ab2
Tonko27 <tonkotsu4127@gmail.com>
2021-08-19 09:25:47 +0000

    [Finish]4-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'fa4abfdafe0da413d1130c4c370833c3681cd67c84067ef1419dfdcfc2a9d0afc59a503a26e2848325e8b9b22f182f60d9bc719399f4916ceee762edd13ccaf3'
